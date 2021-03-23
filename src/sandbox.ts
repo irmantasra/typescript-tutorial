b5e9da3937ce36363c076c2267385ed368592bb2
irmantas <irmantasra@gmail.com>
2021-03-23 22:58:53 +0200

    added

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,2 +1,19 @@
-console.log('testing');
+let greet: Function;
 
+greet = () => {
+  console.log("hello, world");
+};
+
+greet();
+
+const add = (a: number, b: number, c: number = 10) => {
+  console.log(a + b + c);
+};
+
+add(5, 10);
+
+const minus = (a: number, b: number): number => {
+  return a + b;
+};
+
+let result: number = minus(10, 7);
