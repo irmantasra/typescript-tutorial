8f1306ca1cb820ea2d3ef5c1e5731487685ef501
Irmantas <irmantas@simbiotecha.lt>
2021-03-24 10:43:01 +0200

    add

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,22 +1,14 @@
 "use strict";
-// let greet: Function;
-// example 1
 var greet;
 greet = function (name, greeting) {
-    console.log(name + " says " + greeting);
+    console.log(name + " " + greeting);
 };
-// example 2
 var calc;
 calc = function (numOne, numTwo, action) {
-    if (action === 'add') {
+    if (action === "add") {
         return numOne + numTwo;
     }
     else {
         return numOne - numTwo;
     }
-};
-// example 3
-var logDetails;
-logDetails = function (ninja) {
-    console.log(ninja.name + " is " + ninja.age + " years old");
 };
